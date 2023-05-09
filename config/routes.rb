f9ed39b330d6a2bce7bb4bd1853bffc8ae2d7948
AMarceloRamos <adrianramosv@hotmail.com>
2023-05-09 14:00:37 -0400

    agregamos cdn de bootstraps

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-
+  get "news/index"
 
   # root "articles#index"
 
-  get "news/index"
+  root "news#index"
 end
